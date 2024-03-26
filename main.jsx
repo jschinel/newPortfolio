d26ef82c7542a444165f60916e257b70af09e26e
Jon Schinella <jschinel@alumni.nmu.edu>
2024-03-26 12:38:06 -0400

    Test

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom";
-import App from './components/App'
-import '../index.css'
+import App from './src/components/App'
+import './index.css'
 
 
 
