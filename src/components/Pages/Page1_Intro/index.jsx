295ca7f867c5a7a20a0ed4e9996d4c168cceadf1
Jon Schinella <jschinel@alumni.nmu.edu>
2024-03-26 15:23:22 -0400

    Fix

--- src/components/Pages/Page1_Intro/index.jsx
+++ src/components/Pages/Page1_Intro/index.jsx
@@ -7,7 +7,7 @@ export default function Author()
         <>
             <div className="">
                 <h1 className="text-6xl pb-10">Author: Jon Schinella</h1>
-                <img className=" scale-90 rounded-2xl" src='dist/databaseManager.png' alt="" />
+                <img className=" scale-90 rounded-2xl" src='/databaseManager.png' alt="" />
                 <h1 className="pt-0">A dedicated enginner with years of technological experience that is currently pursuing employment within software development field.</h1>
             </div>
         </>
