e119af6deeecc70cf1f2028becb633dfb8d814f9
Jon Schinella <jschinel@alumni.nmu.edu>
2024-03-31 19:42:12 -0400

    Fixing Typos

--- src/components/Pages/Page9_Personal/index.jsx
+++ src/components/Pages/Page9_Personal/index.jsx
@@ -7,11 +7,11 @@ export default function Personal()
             <div className="flex flex-col h-[80%] w-full justify-start overflow-y-scroll ">
                 <div className="text-left px-5 pt-20">
                     <h1 className="text-4xl underline bold">Personal</h1>
-                    <p className="pt-5"> I grew up in a small town tucked away in the woods of the Upper Peninsula of Michigan. After shoveling snow for 22 years, i finally ventured off and moved downstate but shortly after i was hopping on flights to travel the world for work. I've learned over the years that every place has its perks and everywhere has great people but sooner or later you will aquire the itch to get back home.</p>
+                    <p className="pt-5"> I grew up in a small town tucked away in the woods of the Upper Peninsula of Michigan. After shoveling snow for 22 years, i finally ventured off and moved downstate but shortly after i was hopping on flights to travel the world for work. I've learned over the years that every place has its perks and everywhere has great people but sooner or later you will acquire the itch to get back home.</p>
                 </div>
                 <div className="text-left px-5 pt-8">
                     <h1 className="text-4xl underline bold">Hobbies</h1>
-                    <p className="pt-5"> Ever since i could walk I've been tromping through the woods or jumping in the rivers. No matter where i go i will always find a way to be  outside whether its hunting, fishing, or just relaxing at a campground.</p>
+                    <p className="pt-5"> Ever since i could walk I've been tromping through the woods or jumping in the rivers. No matter where i go i will always find a way to be outside whether its hunting, fishing, or just relaxing at a campground.</p>
                 </div>
             </div>
         </>
